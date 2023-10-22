7a5690399dcefa074f714a0de77590625fb92050
kerolmrts <kerolayne.viana@al.infnet.edu.br>
2023-10-22 11:37:04 -0300

    fix bug

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicRuntimeConfig: {
-    TMDB_API_KEY: process.env.TMDB_API_KEY,
-    TMDB_AUTH_TOKEN: process.env.TMDB_AUTH_TOKEN,
+    TMDB_API_KEY: process.env.NEXT_PUBLIC_TMDB_API_KEY,
+    TMDB_AUTH_TOKEN: process.env.NEXT_PUBLIC_TMDB_AUTH_TOKEN,
   },
 };
